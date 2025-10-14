274026d61a47910a519a37fcadde86ba2a03fe6c
Abigail <131455685+ST10045251@users.noreply.github.com>
2025-10-14 23:09:42 +0200

    Made changes to the Home screen and added float buttons, making it aesthetically pleasing

--- Forms/HomeForm.cs
+++ Forms/HomeForm.cs
@@ -23,5 +23,10 @@ namespace TableValleyMunicipalApp.Forms
             mainForm.Show();
             this.Hide();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
