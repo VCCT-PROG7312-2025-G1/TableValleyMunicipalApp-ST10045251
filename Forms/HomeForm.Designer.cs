14ab60578cc0703656683527a5d7a7e42d0cee8e
Abigail <131455685+ST10045251@users.noreply.github.com>
2025-10-15 23:50:54 +0200

    Made UI changes

--- Forms/HomeForm.Designer.cs
+++ Forms/HomeForm.Designer.cs
@@ -31,7 +31,13 @@
             this.flpIssues = new System.Windows.Forms.FlowLayoutPanel();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            this.flpIssues.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblTitle
@@ -64,6 +70,7 @@
             // btnTrackProgress
             // 
             this.btnTrackProgress.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(128)))), ((int)(((byte)(173)))));
+            this.btnTrackProgress.ForeColor = System.Drawing.SystemColors.ControlDark;
             this.btnTrackProgress.Location = new System.Drawing.Point(650, 364);
             this.btnTrackProgress.Name = "btnTrackProgress";
             this.btnTrackProgress.Size = new System.Drawing.Size(187, 50);
@@ -82,6 +89,7 @@
             this.btnStayInformed.TabIndex = 4;
             this.btnStayInformed.Text = "Stay Informed";
             this.btnStayInformed.UseVisualStyleBackColor = false;
+            this.btnStayInformed.Click += new System.EventHandler(this.btnStayInformed_Click);
             // 
             // picLogo
             // 
@@ -107,28 +115,67 @@
             // 
             this.flpIssues.AutoScroll = true;
             this.flpIssues.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(128)))), ((int)(((byte)(173)))));
-            this.flpIssues.Location = new System.Drawing.Point(48, 237);
+            this.flpIssues.Controls.Add(this.label3);
+            this.flpIssues.Location = new System.Drawing.Point(48, 201);
             this.flpIssues.Name = "flpIssues";
-            this.flpIssues.Size = new System.Drawing.Size(187, 178);
+            this.flpIssues.Size = new System.Drawing.Size(187, 214);
             this.flpIssues.TabIndex = 6;
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.AutoScroll = true;
             this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(128)))), ((int)(((byte)(173)))));
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(349, 236);
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(349, 201);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(187, 178);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(187, 213);
             this.flowLayoutPanel1.TabIndex = 8;
             // 
             // flowLayoutPanel2
             // 
             this.flowLayoutPanel2.AutoScroll = true;
             this.flowLayoutPanel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(128)))), ((int)(((byte)(173)))));
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(651, 237);
+            this.flowLayoutPanel2.Controls.Add(this.label4);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(651, 201);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(187, 178);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(187, 214);
             this.flowLayoutPanel2.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 120);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "View any upcoming commuinty\r\nmeetings, events or important \r\npublic notices in yo" +
+    "ur area.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 160);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Log any problem whether it be, water leaks, \r\npotholes or power outages, \r\ndirect" +
+    "ly to your municipailty for a faster resolution.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkGray;
+            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 105);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Check the progress of your reported issues \r\nand see which have been resolved \r\nb" +
+    "y the municipality.";
             // 
             // HomeForm
             // 
@@ -146,6 +193,12 @@
             this.Name = "HomeForm";
             this.Text = "Home - Table Valley";
             ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            this.flpIssues.ResumeLayout(false);
+            this.flpIssues.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -155,5 +208,8 @@
         private System.Windows.Forms.FlowLayoutPanel flpIssues;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
---
+++ Forms/StayInformed.Designer.cs
@@ -0,0 +1,214 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace TableValleyMunicipalApp.Forms
+{
+    partial class StayInformed
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.GroupBox grpAddEvent;
+        private System.Windows.Forms.Label lblEventName;
+        private System.Windows.Forms.TextBox txtEventName;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.DateTimePicker dtpEventDate;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.Label lblLocation;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Button btnAddevent;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.FlowLayoutPanel flpEvents;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ComboBox cmbSort;
+        private System.Windows.Forms.Label lblSort;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private void InitializeComponent()
+        {
+            this.grpAddEvent = new System.Windows.Forms.GroupBox();
+            this.lblEventName = new System.Windows.Forms.Label();
+            this.txtEventName = new System.Windows.Forms.TextBox();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.dtpEventDate = new System.Windows.Forms.DateTimePicker();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.lblLocation = new System.Windows.Forms.Label();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.btnAddevent = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.flpEvents = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.cmbSort = new System.Windows.Forms.ComboBox();
+            this.lblSort = new System.Windows.Forms.Label();
+
+            //grpAddEvent
+            this.grpAddEvent.BackColor = Color.FromArgb(255, 253, 245);
+            this.grpAddEvent.Controls.Add(this.lblEventName);
+            this.grpAddEvent.Controls.Add(this.txtEventName);
+            this.grpAddEvent.Controls.Add(this.lblDate);
+            this.grpAddEvent.Controls.Add(this.dtpEventDate);
+            this.grpAddEvent.Controls.Add(this.lblCategory);
+            this.grpAddEvent.Controls.Add(this.cmbCategory);
+            this.grpAddEvent.Controls.Add(this.lblLocation);
+            this.grpAddEvent.Controls.Add(this.txtLocation);
+            this.grpAddEvent.Controls.Add(this.lblDescription);
+            this.grpAddEvent.Controls.Add(this.txtDescription);
+            this.grpAddEvent.Controls.Add(this.btnAddevent);
+            this.grpAddEvent.Controls.Add(this.btnBack);
+            this.grpAddEvent.Location = new Point(12, 12);
+            this.grpAddEvent.Name = "Add Event";
+            this.grpAddEvent.Size = new Size(560, 310);
+            this.grpAddEvent.TabIndex = 0;
+            this.grpAddEvent.TabStop = false;
+            this.grpAddEvent.Text = "Add any Local Event";
+
+            //lblEventName
+            this.lblEventName.Location = new Point(20, 50);
+            this.lblEventName.Name = "EventName";
+            this.lblEventName.Size = new Size(80, 20);
+            this.lblEventName.Text = "Enter Event Name:";
+
+            //txtEventName
+            this.txtEventName.Location = new Point(110, 47);
+            this.txtEventName.Size = new Size(200, 20);
+
+            //lblDate
+            this.lblDate.Location = new Point(20, 58);
+            this.lblDate.Text = "Date:";
+
+            //dtpEventDate
+            this.dtpEventDate.Location = new Point(110, 82);
+            this.dtpEventDate.Size = new Size(200, 20);
+
+            //lblCategory
+            this.lblCategory.Location = new Point(20, 120);
+            this.lblCategory.Text = "Category:";
+
+            //cmbCategory
+            this.cmbCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCategory.Items.AddRange(new object[] {
+                "Environment",
+                "Community",
+                "Sports",
+                "Education",
+                "Safety",
+                "Utilities"
+            });
+            this.cmbCategory.Location = new Point(110, 117);
+            this.cmbCategory.Size = new Size(150, 21);
+
+            //lblLocation
+            this.lblLocation.Location = new Point(20, 155);
+            this.lblLocation.Text = "Location:";
+
+            //txtLocation
+            this.txtLocation.Location = new Point(110, 152);
+            this.txtLocation.Size = new Size(200, 20);
+
+            //lblDescription
+            this.lblDescription.Location = new Point(20, 190);
+            this.lblDescription.Text = "Description:";
+
+            //txtDescription
+            this.txtDescription.Location = new Point(110, 187);
+            this.txtDescription.Size = new Size(420, 60);
+            this.txtDescription.Multiline = true;
+
+            //btnAddEvent
+            this.btnAddevent.BackColor = Color.FromArgb(180, 100, 144);
+            this.btnAddevent.FlatStyle = FlatStyle.Flat;
+            this.btnAddevent.ForeColor = Color.White;
+            this.btnAddevent.Location = new Point(450, 265);
+            this.btnAddevent.Size = new Size(80, 25);
+            this.btnAddevent.Text = "Add Event";
+            this.btnAddevent.UseVisualStyleBackColor = false;
+            this.btnAddevent.Click += new System.EventHandler(this.btnAddEvent_Click);
+
+            //btnBack
+            this.btnBack.BackColor = Color.FromArgb(180, 100, 144);
+            this.btnBack.FlatStyle = FlatStyle.Flat;
+            this.btnBack.ForeColor = Color.White;
+            this.btnBack.Location = new Point(6, 19);
+            this.btnBack.Size = new Size(80, 25);
+            this.btnBack.Text = "Back";
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+
+            //txtSearch
+            this.txtSearch.Location = new Point(12, 335);
+            this.txtSearch.Size = new Size(180, 20);
+            this.txtSearch.Text = "Search all events...";
+            this.txtSearch.ForeColor = Color.Gray;
+            this.txtSearch.GotFocus += (s, e) =>
+            {
+                if (txtSearch.Text == "Search all events...")
+                {
+                    txtSearch.Text = "";
+                    txtSearch.ForeColor = Color.Black;
+                }
+            };
+            this.txtSearch.LostFocus += (s, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(txtSearch.Text))
+                {
+                    txtSearch.ForeColor = Color.Gray;
+                }
+
+            };
+
+            //btnSearch
+            this.btnSearch.BackColor = Color.FromArgb(180,110,144);
+            this.btnSearch.FlatStyle = FlatStyle.Flat;
+            this.btnSearch.ForeColor = Color.White;
+            this.btnSearch.Location = new Point(200, 333);
+            this.btnSearch.Size = new Size(80, 25);
+            this.btnSearch.Text = "Search:";
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+
+            //lblSort
+            this.lblSort.Location = new Point(300, 335);
+            this.lblSort.Text = "Sort By:";
+            
+            //cmbSort
+            this.cmbSort.DropDownStyle=ComboBoxStyle.DropDownList;
+            this.cmbSort.Items.AddRange(new object[] { "Date", "Name", "Category" });
+            this.cmbSort.Location = new Point(350, 333);
+            this.cmbSort.Size = new Size(100, 21);
+            this.cmbSort.SelectedIndexChanged += new System.EventHandler(this.comboBoxSort_SelectedIndexChanged);
+
+            //flpEvents
+            this.flpEvents.AutoScroll = true;
+            this.flpEvents.BackColor = Color.FromArgb(254, 253, 245);
+            this.flpEvents.Location = new Point(12, 370);
+            this.flpEvents.Size = new Size(560, 170);
+
+            //StayInformed
+            this.BackColor = Color.FromArgb(254,253,245);
+            this.ClientSize = new Size(584, 560);
+            this.Controls.Add(this.grpAddEvent);
+            this.Controls.Add(this.txtSearch);
+                this.Controls.Add(this.btnSearch);
+                this.Controls.Add(this.lblSort);
+                this.Controls.Add(this.cmbSort);
+                this.Controls.Add(this.flpEvents);
+            this.Name = "Events";
+            this.grpAddEvent.ResumeLayout(false);
+            this.grpAddEvent.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
