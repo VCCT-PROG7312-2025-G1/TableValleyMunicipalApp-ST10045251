bdf8a7486f3f81c576a2a0bdc975839464e8e837
Abigail <131455685+ST10045251@users.noreply.github.com>
2025-10-15 23:51:13 +0200

    added events \model

---
+++ Models/Events.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableValleyMunicipalApp.Models
+{
+    internal class Events
+    {
+        public string Id{ get; set; } 
+        public string EventName{ get; set; }
+        public  DateTime Date{ get; set; }
+        public string Category{ get; set; }
+        public string Location{ get; set; }
+        public string Description{ get; set; }
+    }
+}
+  
